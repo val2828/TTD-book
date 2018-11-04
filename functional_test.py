9f5567a615c55c67113bd7c595d8a0656b687f62
Valentin Dmitrochenko <val2828@tut.by>
2018-11-04 17:11:19 +0100

    "Refactoring of the functional test"

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest (unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Customer visits the web-stites homepage
         self.browser.get('http://localhost:8000')
@@ -33,18 +38,21 @@ class NewVisitorTest (unittest.TestCase):
         # input text " as an item in the To-Do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: buy peacock feathers')
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         # There is another text box whih invites to add another item. 
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         # Another update occurs
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # A unique URL is generated for the customer with some reference text to it
 
         # Customer visits the URL and sees his list in-tact
-
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
