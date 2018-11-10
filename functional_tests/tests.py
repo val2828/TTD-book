36255eca5f3c6a58b48ec69f801a847ea72365ac
Valentin Dmitrochenko <val2828@tut.by>
2018-11-10 17:08:57 +0100

    Checkpoint before 6th chapter

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 import time
 
-class NewVisitorTest (unittest.TestCase):
+class NewVisitorTest (LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest (unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Customer visits the web-stites homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
     
         # Notice the header to contain term To-Do
         self.assertIn('To-Do', self.browser.title)
